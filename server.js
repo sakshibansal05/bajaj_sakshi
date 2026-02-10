c5d7378d09edb98a3208529153828f2ebe3bcf5a
Sakshi bansal <sakshinohar@gmail.com>
2026-02-10 11:46:34 +0530

    / api

--- server.js
+++ server.js
@@ -28,6 +28,13 @@ function errorResponse(msg){
     }
 }
 
+app.get("/",(req,res)=>{
+   res.json({
+      message:"Bajaj BFHL API is running"
+   });
+});
+
+
 app.get("/health",(req,res)=>{
     res.status(200).json({
         is_success:true,
