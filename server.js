a1f932dbec1f29cf32f19cbafe62a1932a3a3c30
Sakshi bansal <sakshinohar@gmail.com>
2026-02-10 11:32:02 +0530

    added vercel config

--- server.js
+++ server.js
@@ -140,6 +140,5 @@ app.post("/bfhl",async(req,res)=>{
 
 const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
+
